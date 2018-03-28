a80ad93fcd3ba62687ffce740c7f0b3e496b0e80
Baxter Grace <baxter.grace@kapost.com>
2018-03-27 22:26:30 -0600

    use virtualenvwrapper commands
    remove set -eou pipefail so that virtualenvwrapper can set unset variables it needs.
    this lets us use virtualenvwrapper commands to properly update python without
    breaking pip

--- vewpu.sh
+++ vewpu.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-set -eou pipefail
+# make virtualenvwrapper commands available
+source `which virtualenvwrapper.sh`
 
 new_python_version="$( ${VIRTUALENVWRAPPER_PYTHON} --version )"
 
@@ -8,8 +9,17 @@ new_python_version="$( ${VIRTUALENVWRAPPER_PYTHON} --version )"
 cd "$PROJECT_HOME"
 for project in */; do
   current_version="$( "${WORKON_HOME}"/"${project}"/bin/python --version )"
+  # recreate virtualenv if versions don't match
   if [[ "$current_version" != "$new_python_version" ]]; then
-    virtualenv --python="$VIRTUALENVWRAPPER_PYTHON" "${WORKON_HOME}/${project}"
+    echo "changing to ${project} directory"
+    workon "$project"
+    echo "deactivating virtualenv"
+    deactivate
+    rmvirtualenv "$project"
+    echo "creating new virtualenv"
+    mkvirtualenv "$project"
+    echo "setting virtualenv to project"
+    setvirtualenvproject
   else
     echo "${project} is already at the latest installed python: ${new_python_version}"
   fi
